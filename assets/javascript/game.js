ed8b636c595c1d1bbee61bfbf8e98e3e6c205cd0
Kangxian Xie <Gary@Kangxians-MacBook-Pro.local>
2018-02-17 00:14:51 -0800

    this is my commit of the game hangman

---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+var wrongGuess = document.getElementById("wrong-guesses");
+
+var word_blanks = document.getElementById("word_blanks");
+
+var guess_left = document.getElementById("guesses-left"); 
+
+var wins = document.getElementById("win-counter");
+
+var winsNum = 0;
+
+var losses = document.getElementById("loss-counter");
+
+var lossNum = 0;
+
+var names_to_guess = ["michael","gary","brian","luke","anthony","kyle"];
+
+var randomNumber= Math.floor(Math.random() * 6);
+
+var namewanted = names_to_guess[randomNumber];
+
+var nameWantedArray = new Array(...namewanted);
+
+var namelength = namewanted.length;
+
+var wrongGuess_text = "";
+
+word_blanks_str = "";
+
+var name_str_array;
+
+var chance = 9;
+
+for(var i=0;i<namelength;i++){
+    word_blanks_str += "_ ";
+
+}
+
+var word_blanks_str_array = new Array(...word_blanks_str);
+var word_blank = document.getElementById("word-blank");
+word_blank.innerHTML=word_blanks_str;
+
+var correctness = false;
+
+document.onkeyup = function(event) {
+    if(chance == 0){
+        lossNum+=1;
+        losses.innerHTML = lossNum;
+        return;
+        alert("you ran out of chances!");
+    }
+    correctness = false;
+    var keyP = event.key;
+    for( i=0; i<names_to_guess.length; i++){
+        if( keyP === nameWantedArray[i] ){
+            word_blanks_str_array[0+2*i]=keyP;
+            word_blanks_str = word_blanks_str_array.join("");
+            correctness = true;
+        }
+    }
+    word_blank.innerHTML = word_blanks_str; 
+    //console.log(nameWantedArray);
+    if(correctness == false){
+        chance-=1;
+        
+        if(!wrongGuess_text.includes(keyP)){
+            wrongGuess_text+=keyP;
+            wrongGuess_text+=" ";
+        }
+        wrongGuess.innerHTML = wrongGuess_text;
+    }
+    guess_left.innerHTML = chance;
+
+    if(!word_blanks_str.includes("_")){
+        
+        winsNum+=1;
+        wins.innerHTML = winsNum;
+    }
+    
+  };
+
+  
+
+
+
+ 
